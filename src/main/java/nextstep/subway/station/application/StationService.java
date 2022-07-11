7af70dfa510750ee28a1872a9d90a274f1b81f20
jeongjaeeom <jeongjae.eom@gmail.com>
2022-07-11 17:11:42 +0900

    feat($compile): Redis 및 운영 환경 설정
    Redis 및 운영 환경 설정

--- src/main/java/nextstep/subway/station/application/StationService.java
+++ src/main/java/nextstep/subway/station/application/StationService.java
@@ -1,47 +1,61 @@
 package nextstep.subway.station.application;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import nextstep.subway.station.domain.Station;
 import nextstep.subway.station.domain.StationRepository;
 import nextstep.subway.station.dto.StationRequest;
 import nextstep.subway.station.dto.StationResponse;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class StationService {
-    private StationRepository stationRepository;
 
-    public StationService(StationRepository stationRepository) {
-        this.stationRepository = stationRepository;
-    }
+  private StationRepository stationRepository;
 
-    public StationResponse saveStation(StationRequest stationRequest) {
-        Station persistStation = stationRepository.save(stationRequest.toStation());
-        return StationResponse.of(persistStation);
-    }
+  public StationService(StationRepository stationRepository) {
+    this.stationRepository = stationRepository;
+  }
 
-    @Transactional(readOnly = true)
-    public List<StationResponse> findAllStations() {
-        List<Station> stations = stationRepository.findAll();
+  @CacheEvict(value = "stations", allEntries = true)
+  public StationResponse saveStation(StationRequest stationRequest) {
+    Station persistStation = stationRepository.save(stationRequest.toStation());
+    return StationResponse.of(persistStation);
+  }
 
-        return stations.stream()
-                .map(StationResponse::of)
-                .collect(Collectors.toList());
-    }
+  @Transactional(readOnly = true)
+  @Cacheable(value = "stations", key = "#root.methodName")
+  public List<StationResponse> findAllStations() {
+    List<Station> stations = stationRepository.findAll();
 
-    public void deleteStationById(Long id) {
-        stationRepository.deleteById(id);
-    }
+    return stations.stream()
+        .map(StationResponse::of)
+        .collect(Collectors.toList());
+  }
 
-    public Station findStationById(Long id) {
-        return stationRepository.findById(id).orElseThrow(RuntimeException::new);
-    }
+  @Caching(
+      evict = {
+          @CacheEvict(value = "stations", allEntries = true),
+          @CacheEvict(value = "station", key = "#id")
+      }
+  )
+  public void deleteStationById(Long id) {
+    stationRepository.deleteById(id);
+  }
 
-    public Station findById(Long id) {
-        return stationRepository.findById(id).orElseThrow(RuntimeException::new);
-    }
+
+  @Cacheable(value = "station", key = "#id")
+  public Station findStationById(Long id) {
+    return stationRepository.findById(id).orElseThrow(RuntimeException::new);
+  }
+
+  @Cacheable(value = "station", key = "#id")
+  public Station findById(Long id) {
+    return stationRepository.findById(id).orElseThrow(RuntimeException::new);
+  }
 }
