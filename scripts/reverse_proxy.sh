1a194afa1e4432448f4e4bd08628efd735808c39
orgojy <giveawesome@gmail.com>
2022-09-06 01:24:23 +0900

    Update reverse_proxy.sh to build image for reverse_proxy

--- scripts/reverse_proxy.sh
+++ scripts/reverse_proxy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 docker stop proxy && docker rm proxy
+docker build -t nextstep/reverse-proxy:0.0.2 .
 docker run -d -p 80:80 -p 443:443 --name proxy -v /var/log/nginx:/var/log/nginx nextstep/reverse-proxy:0.0.2
