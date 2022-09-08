3b5aff138bc16ade95bcc56b3dfc397b1c65eb72
orgojy <giveawesome@gmail.com>
2022-09-08 23:10:46 +0900

    Update SubwayApplication to remove unused logic for invalidating cache

--- src/main/java/nextstep/subway/SubwayApplication.java
+++ src/main/java/nextstep/subway/SubwayApplication.java
@@ -2,9 +2,6 @@ package nextstep.subway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.cache.CacheManager;
-import org.springframework.context.event.EventListener;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -13,20 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 public class SubwayApplication {
 
-    private final CacheManager cacheManager;
-
-    public SubwayApplication(CacheManager cacheManager) {
-        this.cacheManager = cacheManager;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(SubwayApplication.class, args);
-    }
-
-    @EventListener
-    public void onApplicationEvent(ApplicationReadyEvent event) {
-        cacheManager.getCacheNames()
-                .parallelStream()
-                .forEach(n -> cacheManager.getCache(n).clear());
     }
 }
