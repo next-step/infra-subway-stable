efe2f8786f76c80e3ca95211bd09334048cf23f3
orgojy <giveawesome@gmail.com>
2022-09-08 21:43:41 +0900

    Update SubwayApplication.java to invalidate redis cache

--- src/main/java/nextstep/subway/SubwayApplication.java
+++ src/main/java/nextstep/subway/SubwayApplication.java
@@ -2,6 +2,9 @@ package nextstep.subway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.event.EventListener;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,8 +13,20 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 public class SubwayApplication {
 
+    private final CacheManager cacheManager;
+
+    public SubwayApplication(CacheManager cacheManager) {
+        this.cacheManager = cacheManager;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SubwayApplication.class, args);
     }
 
+    @EventListener
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        cacheManager.getCacheNames()
+                .parallelStream()
+                .forEach(n -> cacheManager.getCache(n).clear());
+    }
 }
